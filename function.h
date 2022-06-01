cca597734b82a01bdb5a2598c4b9230402e3a673
Chien <chien567@163.com>
2022-06-01 12:30:20 +0800

    5.1

--- function.h
+++ function.h
@@ -3,37 +3,21 @@
 #include <stdlib.h>
 #define N 30
 #define S 10
-#define W 6
-
-typedef struct student
-{
-    long num;
-    char name[S];
-    int score[W];
-}STUDENT;
-
-STUDENT stu[N];
-STUDENT* pt = stu;
 
 
 /*函数原型声明*/
 void Menu();//菜单
-void Input(STUDENT stu[], int n, int m);//录入数据
-void TotalAverage_Subject(STUDENT stu[], int n, int m);//课程总分和平均分
-void TotalAverage_Student(STUDENT stu[], int n, int m);
-void Seqencing_Score(STUDENT stu[], int n, int m, int (*f)(int a, int b));//成绩排序
-void Seqencing_Num(STUDENT stu[], int n, int m);//学号排序
-void Dictionary_Order(STUDENT stu[], int n, int m);//字典顺序排序
-void LookupBy_Num(STUDENT stu[], int n, int m);//按学号查找
-void LookupBy_Name(STUDENT stu[], int n, int m);//按姓名查找
-void Statistic_Analysis(STUDENT stu[], int n, int m);//数据分析
-void List_Record(STUDENT stu[], int n, int m, int (*f)(int a, int b));
-void Total(STUDENT stu[], int sum[N], int n, int m);
-void Grade_Table(STUDENT stu[], int sum[], int n, int m);
-void Swap(int* x, int* y, long* a, long* b, char* j, char* k,int *c,int *d);//交换函数
+void Input(long num[], int score[], char name[][S], int n);//录入数据
+void TotalAverage(int score[], int n);//总分和平均分
+void Seqencing(long num[], int score[], char name[][S], int n, int (*f)(int a, int b));//成绩排序
+void Seqencing2(long num[], int score[], char name[][S], int n);//学号排序
+void DictionaryOrder(long num[], int score[], char name[][S], int n);//字典顺序排序
+void LookupByNum(long num[], int score[], int n,  char name[][S]);//按学号查找
+void LookupByName(long num[], int score[], int n,  char name[][S]);//按姓名查找
+void StatisticAnalysis(int score[], int n);//数据分析
+void Swap(int* x, int* y, long* a, long* b, char* j, char* k);//交换函数
 int Ascending(int a, int b);//返回a<b
 int Descending(int a, int b);//返回a>b
-
 
 /*函数定义*/
 void Menu()
@@ -42,16 +26,15 @@ void Menu()
         printf("=");
     printf("\n");
     printf("1. Input record\n");
-    printf("2. Calculate total and average score of every course\n");
-    printf("3. Calculate total and average score of every student\n");
-    printf("4. Sort in descending order by total score of every student\n");
-    printf("5. Sort in asending order by total score of every student\n");
-    printf("6. Sort in ascending order by number\n");
-    printf("7. Sort in dictionary order by name\n");
-    printf("8. Search by number\n");
-    printf("9. Search by name\n");
-    printf("10.Statistic analysis\n");
-    printf("11.	List record\n");
+    printf("2. Caculate total and average score of course\n");
+    printf("3. Sort in descending order by score\n");
+    printf("4. Sort in ascending order by score\n");
+    printf("5. Sort in ascending order by numberme\n");
+    printf("6. Sort in dictionary order by name\n");
+    printf("7. Search by number\n");
+    printf("8. Search by name\n");
+    printf("9. Statistic analysis\n");
+    printf("10. List record\n");
     printf("0.Exit\n");
     for (int i = 0; i <= 45; i++)
         printf("=");
@@ -59,145 +42,103 @@ void Menu()
     printf("please enter your choice:\n");
 }
 
-void Input(STUDENT stu[], int n, int m)//录入数据
+void Input(long num[], int score[], char name[][S], int n)//录入数据
 {
-    //memset(score, 0, sizeof(score)); //初始化成绩数组
+    memset(score, 0, sizeof(score)); //初始化成绩数组
     printf("Please enter the ID,name and score：\n");
-    printf("ID\tname\t");
-    for (int i = 0; i < m; i++)
-    {
-        printf("subject%d\t", i + 1);
-    }
-    printf("\n");
+    printf("ID\tname\tscore\n");
     for (int i = 0; i < n; i++)
     {
-        scanf("%ld %s", &stu[i].num,&stu[i].name);
-        for(int j=0;j<m;j++)
-        scanf("%d",&stu[i].score[j]);
+        scanf("%ld %s %d", &num[i], name[i], &score[i]);
     }
     printf(".......\n");
     printf("After data entry:\n");
 }
 
-void TotalAverage_Subject(STUDENT stu[], int n, int m)//计算课程总分和平均分
+void TotalAverage(int score[], int n)//计算总分和平均分
 {
-    int i, j, sum[W];
-    for (i = 0; i < m; i++)
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        sum[i] = 0;
-        for (j = 0; j < n; j++)
-        {
-            sum[i] =sum[i]+stu[j].score[i];
-        }
+        sum = sum + score[i];
     }
-    printf("\t\tTotal\tAverage\n");
-    for (i = 0; i < m; i++)
-    {
-        printf("Subject%d:\t%d\t%0.2f\n", i + 1, sum[i], (float)sum[i] / n);
-    }
+    printf("Total score:%d\nAverage score:%.2f\n", sum, (double)sum / n);
 }
 
-void TotalAverage_Student(STUDENT stu[], int n, int m)
+void Seqencing(long num[], int score[], char name[][S], int n, int (*f)(int a, int b))//排序
 {
-    int i, sum[N];
-    Total(stu, sum, n, m);
-    printf("ID\tName\tTotal\tAverage\n");
+    int i, j, k;
+    for (i = 0; i < n - 1; i++)
+    {
+        k = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if ((*f)(score[j], score[k]))
+                k = j;
+        }
+        printf("%d", score[i]);
+        if (k != i)
+        {
+            Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k]);
+        }
+    }
+    printf("Score\tName\tID\n");
     for (i = 0; i < n; i++)
-    {
-        printf("%ld\t%s\t%d\t%0.2f\n", stu[i].num, stu[i].name, sum[i], (float)sum[i] / n);
-    }
+        printf("%d\t%s\t%ld\n", score[i], name[i], num[i]);
 }
 
-void Seqencing_Score(STUDENT stu[], int n, int m, int (*f)(int a, int b))//排序
+void Seqencing2(long num[], int score[], char name[][S], int n)//排序
 {
-    int i, j, k, sum[N],temp_;
-    STUDENT temp;
-    Total(stu, sum, n,m);
+    int i, j, k;
     for (i = 0; i < n - 1; i++)
     {
         k = i;
         for (j = i + 1; j < n; j++)
         {
-            if ((*f)(sum[j], sum[k]))
+            if (num[j] < num[k])
                 k = j;
         }
         if (k != i)
         {
-            temp = stu[i];
-            stu[i] = stu[k];
-            stu[k] = temp;
-            temp_ = sum[i];
-            sum[i] = sum[k];
-            sum[k] = temp_;
-            //Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k],&sum[i],&sum[k]);
+            Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k]);
         }
     }
-    Grade_Table(stu, sum, n, m);
+    printf("ID\tName\tScore\n");
+    for (i = 0; i < n; i++)
+        printf("%ld\t%s\t%d\n", num[i], name[i], score[i]);
 }
 
-void Seqencing_Num(STUDENT stu[], int n, int m)//排序
+void DictionaryOrder(long num[], int score[], char name[][S], int n)
 {
-    int i, j, k,sum[N],temp_;
-    STUDENT temp;
-    Total(stu, sum, n, m);
+    int i, j, k;
     for (i = 0; i < n - 1; i++)
     {
         k = i;
         for (j = i + 1; j < n; j++)
         {
-            if (stu[j].num < stu[k].num)
+            if (strcmp(name[j], name[k]) > 0)
                 k = j;
         }
         if (k != i)
         {
-            temp = stu[i];
-            stu[i] = stu[k];
-            stu[k] = temp;
-            temp_ = sum[i];
-            sum[i] = sum[k];
-            sum[k] = temp_;
-            //Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k],&sum[i],&sum[k]);
-        }
-    } 
-    Grade_Table(stu, sum, n, m);
-}
-
-void Dictionary_Order(STUDENT stu[], int n, int m)
-{
-    int i, j, k,sum[N],temp_;
-    STUDENT temp;
-    Total(stu, sum, n, m);
-    for (i = 0; i < n - 1; i++)
-    {
-        k = i;
-        for (j = i + 1; j < n; j++)
-        {
-            if (strcmp(stu[j].name, stu[k].name) < 0)
-                k = j;
-        }
-        if (k != i)
-        {
-            temp = stu[i];
-            stu[i] = stu[k];
-            stu[k] = temp;
-            temp_ = sum[i];
-            sum[i] = sum[k];
-            sum[k] = temp_;
-          // Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k],&sum[i],&sum[k]);
+            Swap(&score[i], &score[k], &num[i], &num[k], name[i], name[k]);
         }
     }
-    Grade_Table(stu, sum, n, m);
+    printf("Name\tID\tScore\n");
+    for (i = 0; i < n; i++)
+        printf("%s\t%ld\t%d\n", name[i], num[i], score[i]);
 }
 
-void LookupBy_Num(STUDENT stu[], int n, int m)
+
+void LookupByNum(long num[], int score[], int n, char name[][S])
 {
-    long Num_ = 0;
+    long Num_=0;
     int x = -1, i;
     printf("please enter number you want search:\n");
     scanf("%ld", &Num_);
     for (i = 0; i < n; i++)
     {
-        if (Num_ == stu[i].num)
+        if (Num_ == num[i])
         {
             x = i;
         }
@@ -208,30 +149,21 @@ void LookupBy_Num(STUDENT stu[], int n, int m)
     }
     else
     {
-        printf("ID\tname\t");
-        for (i = 0; i < m; i++)
-        {
-            printf("subject%d\t", i + 1);
-        }
-        printf("\n%ld\t%s\t", stu[x].num, stu[x].name);
-        for (i = 0; i < m; i++)
-        {
-            printf("%d\t\t", stu[x].score[i]);
-        }
-        printf("\n");
+        printf("ID\tname\tscore\n");
+        printf("%ld\t%s\t%d\n", num[x], name[x], score[x]);
     }
 }
 
-void LookupBy_Name(STUDENT stu[], int n, int m)
+void LookupByName(long num[], int score[], int n,  char name[][S])
 {
-    char Name_[S]={0};
-    int x = -1, i, r;
+    char Name_[S];
+    int x = -1, i,r;
     printf("please enter name you want search:\n");
     scanf("%s", Name_);
     for (i = 0; i < n; i++)
     {
-        r = strcmp(Name_,stu[i].name);
-        if (!r)
+        r=strcmp(Name_, name[i]);
+        if ( !r)
         {
             x = i;
         }
@@ -242,145 +174,35 @@ void LookupBy_Name(STUDENT stu[], int n, int m)
     }
     else
     {
-        printf("ID\tname\t");
-        for (i = 0; i < m; i++)
-        {
-            printf("subject%d\t", i + 1);
-        }
-        printf("\n%ld\t%s\t", stu[x].num, stu[x].name);
-        for (i = 0; i < m; i++)
-        {
-            printf("%d\t\t",stu[x].score[i]);
-        }
-        printf("\n");
+        printf("name\tID\tscore\n");
+        printf("%s\t%ld\t%d\n", name[x], num[x], score[x]);
     }
 }
 
-void Statistic_Analysis(STUDENT stu[], int n, int m)
+void StatisticAnalysis(int score[], int n)
 {
-    
-    for (int j = 0; j < m; j++)
+    int A=0, B=0, C=0, D=0, E=0;
+    for (int i=0; i < n; i++)
     {
-        int A = 0, B = 0, C = 0, D = 0, E = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (stu[i].score[j] >= 90 && stu[i].score[j] <= 100)
-                A++;
-            else if (stu[i].score[j] >= 80 && stu[i].score[j] <= 89)
-                B++;
-            else if (stu[i].score[j] >= 70 && stu[i].score[j] <= 79)
-                C++;
-            else if (stu[i].score[j] >= 60 && stu[i].score[j] <= 69)
-                D++;
-            else if (stu[i].score[j] >= 0 && stu[i].score[j] <= 59)
-                E++;
-        }
-        printf("subject%d:", j + 1);
-        printf("\tExcellent:\t%.2f%%\n", (double)A / n * 100);
-        printf("\t\tGood:\t%.2f%%\n", (double)B / n * 100);
-        printf("\t\tMedium:\t%.2f%%\n", (double)C / n * 100);
-        printf("\t\tPass:\t%.2f%%\n", (double)D / n * 100);
-        printf("\t\tFailed:\t%.2f%%\n", (double)E / n * 100);
+        if (score[i] >= 90 && score[i] <= 100)
+            A++;
+        if (score[i] >= 80 && score[i] <= 89)
+            B++;
+        if (score[i] >= 70 && score[i] <= 79)
+            C++;
+        if (score[i] >= 60 && score[i] <= 69)
+            D++;
+        if (score[i] >= 0 && score[i] <= 59)
+            E++;
     }
-
+    printf("Excellent:\t%.2f%%\n",(double)A / n * 100);
+    printf("Good:\t%.2f%%\n", (double)B/ n * 100);
+    printf("Medium:\t%.2f%%\n", (double)C / n * 100);
+    printf("Pass:\t%.2f%%\n", (double)D / n * 100);
+    printf("Failed:\t%.2f%%\n", (double)E / n * 100);
 }
 
-void List_Record(STUDENT stu[], int n, int m, int (*f)(int a, int b))
-{
-    int i, j, k, sum[N], temp_;
-    STUDENT temp;
-    Total(stu, sum, n, m);
-    for (i = 0; i < n - 1; i++)
-    {
-        k = i;
-        for (j = i + 1; j < n; j++)
-        {
-            if ((*f)(sum[j], sum[k]))
-                k = j;
-        }
-        if (k != i)
-        {
-            temp = stu[i];
-            stu[i] = stu[k];
-            stu[k] = temp;
-            temp_ = sum[i];
-            sum[i] = sum[k];
-            sum[k] = temp_;
-        }
-    }
-    printf("ID\tName\t");
-    for (i = 0; i < m; i++)
-    {
-        printf("subject%d\t", i + 1);
-    }
-    printf("TotalScore\tAverage\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%ld\t%s\t", stu[i].num, stu[i].name);
-        for (j = 0; j < m; j++)
-        {
-            printf("%d\t\t", stu[i].score[j]);
-        }
-        printf("%d\t\t%0.2f\n", sum[i],(double)sum[i]);
-    }
-    printf("--------------------------------------\n");
-    TotalAverage_Subject(stu, n, m);
-}
 
-void Grade_Table(STUDENT stu[], int sum[], int n, int m)
-{
-    int i, j;
-    printf("ID\tName\t");
-    for (i = 0; i < m; i++)
-    {
-        printf("subject%d\t", i + 1);
-    }
-    printf("TotalScore\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%ld\t%s\t", stu[i].num, stu[i].name);
-        for (j = 0; j < m; j++)
-        {
-            printf("%d\t\t", stu[i].score[j]);
-        }
-        printf("%d\n", sum[i]);
-    }
-}
-
-void Swap(int* x, int* y, long* a, long* b, char* j, char* k,int *c,int *d)
-{
-    int temp0,temp1;
-    long temp2;
-    char temp3[S];
-    temp0 = *c; *c = *d; *d = temp0;
-    temp1 = *x; *x = *y; *y = temp1;
-    temp2 = *a; *a = *b; *b = temp2;
-    strcpy(temp3, j);
-    strcpy(j, k);
-    strcpy(k, temp3);
-}
-
-void Swap_(STUDENT* x, STUDENT* y,int *a,int *b)
-{
-    /*STUDENT z;
-    int c;
-    z = x->stu[];
-    *x = *y;
-    y = z;*/
-}
-
-void Total(STUDENT stu[], int sum[N], int n, int m)
-{
-    int i, j;
-    for (i = 0; i < n; i++)
-    {
-        sum[i] = 0;
-        for (j = 0; j < m; j++)
-        {
-            sum[i] = sum[i] + stu[i].score[j];
-        }
-    }
-}
 
 int Ascending(int a, int b)
 {
@@ -391,5 +213,19 @@ int Descending(int a, int b)
 {
     return a > b;
 }
+
+void Swap(int* x, int* y, long* a, long* b, char* j, char* k)
+{
+    int temp1;
+    long temp2;
+    char temp3[S];
+    temp1 = *x; *x = *y; *y = temp1;
+    temp2 = *a; *a = *b; *b = temp2;
+    strcpy(temp3, j);
+    strcpy(j, k);
+    strcpy(k, temp3);
+}
+
+
 
 #pragma once
--- main.c
+++ main.c
@@ -1,30 +1,13 @@
 #include"function.h"
 
-/*typedef struct student
-{
-    long num;
-    char name[S];
-    int score[W];
-}STUDENT;
-
-STUDENT stu[N];
-STUDENT* pt = stu;*/
-
-
 int main()
 {
-    int n = 0,m=0, order, order1;
-    //long num[N];
-    //char name[N][S];score[N][W],
-
+    int score[N], n = 0, order, order1;
+    long num[N];
+    char name[N][S];
 Menu:
     Menu();
     scanf("%d", &order);
-    while (order < 0 || order>11)
-    {
-        printf("Data error, please re-enter(0-11)\n");
-        scanf("%d", &order);
-    }
     switch (order)
     {
 
@@ -32,22 +15,14 @@ Menu:
     enter:
         printf("Please enter the number of people(No more than %d)：\n", N);
         scanf("%d", &n);
-        while (n<=0||n>N)//检查人数是否合格
+        while (n <= 0 || n > N)//检查人数是否合格
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &n);
         }
-        printf("Please enter the number of test subjects(No more than %d):\n",W);
-        scanf("%d", &m);
-        while (m <= 0 || m > W)//检查科目是否合格
-        {
-            printf("Data error, please re-enter\n");
-            scanf("%d", &m);
-        }
-        Input(stu, n,m);
+        Input(num, score, name, n);
         goto Menu;
         break;
-
 
     case 2:
         if (n == 0)//检查是否录入数据
@@ -55,10 +30,10 @@ Menu:
             printf("You have not entered data.Will jump to enter data\n......\n");
             goto enter;
         }
-        TotalAverage_Subject(stu, n, m);
+        TotalAverage(score, n);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -69,22 +44,20 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 3:
         if (n == 0)//检查是否录入数据
         {
             printf("You have not entered data.Will jump to enter data\n......\n");
             goto enter;
         }
-        TotalAverage_Student(stu,n,m);//
+        Seqencing(num, score, name, n, Descending);//降序
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -95,22 +68,20 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 4:
         if (n == 0)//检查是否录入数据
         {
             printf("You have not entered data.Will jump to enter data\n......\n");
             goto enter;
         }
-        Seqencing_Score(stu, n,m,Descending);//升序
+        Seqencing(num, score, name, n, Ascending);//升序
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -121,22 +92,20 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 5:
         if (n == 0)//检查是否录入数据
         {
             printf("You have not entered data.Will jump to enter data\n......\n");
             goto enter;
         }
-        Seqencing_Score(stu, n, m,Ascending);//升序
+        Seqencing2(num, score, name, n);//升序
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -147,17 +116,15 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 6:
-        Seqencing_Num(stu, n,m);
+        DictionaryOrder(num, score, name, n);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -168,18 +135,15 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
-
     case 7:
-        Dictionary_Order(stu,n,m);
+        LookupByNum(num, score, n, name);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -190,17 +154,15 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);;
+            goto Exit;
             break;
         }
         break;
-
-
     case 8:
-        LookupBy_Num(stu, n, m);
+        LookupByName(num, score, n, name);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -211,17 +173,15 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 9:
-        LookupBy_Name(stu, n,m);
+        StatisticAnalysis(score, n);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -232,19 +192,17 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-
     case 10:
-        Statistic_Analysis(stu, n, m);
+        Seqencing2(num, score, name, n);
         printf("------------------\n");
-       // TotalAverage(score, n);
+        TotalAverage(score, n);
         printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
         scanf("%d", &order1);
-        while (order1 != 1 && order1 != 2)
+        while (order1 != 1 && order != 2)
         {
             printf("Data error, please re-enter\n");
             scanf("%d", &order1);
@@ -255,36 +213,11 @@ Menu:
             goto Menu;
             break;
         case 2:
-            exit(0);
+            goto Exit;
             break;
         }
         break;
-
-    case 11:
-        if (n == 0)//检查是否录入数据
-        {
-            printf("You have not entered data.Will jump to enter data\n......\n");
-            goto enter;
-        }
-        List_Record(stu, n, m,Ascending);
-        printf("==============\n1.Return to menu\n2.Exit program\nPlease enter your choice:\n");//提示
-        scanf("%d", &order1);
-        while (order1 != 1 && order1 !=2)
-        {
-            printf("Data error, please re-enter\n");
-            scanf("%d", &order1);
-        }
-        switch (order1)
-        {
-        case 1:
-            goto Menu;
-            break;
-        case 2:
-            exit(0);
-            break;
-        }
-        break;
-
+    Exit:
     case 0:
         exit(0);
         break;
